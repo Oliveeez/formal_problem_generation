d2170dfee1d8c864d3278f9d2037cda7aec99667
Qi Liu <729170579@qq.com>
2025-07-27 17:27:51 +0800

    Add MiniF2F env

---
+++ data/MiniF2F/lakefile.lean
@@ -0,0 +1,10 @@
+import Lake
+open Lake DSL
+
+require aesop from "/home/ma-user/workspace/fps_pantograph/dependencies/aesop"
+require mathlib from "/home/ma-user/workspace/fps_pantograph/dependencies/mathlib4"
+
+package MiniF2F
+
+@[default_target]
+lean_lib MiniF2F
