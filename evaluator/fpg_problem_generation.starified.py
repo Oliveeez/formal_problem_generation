bbaed958e277c2ffb18c82ee87c18cccf0ff364d
Qi Liu <729170579@qq.com>
2025-09-05 18:11:49 +0800

    Init: staritied-conditioned FPG

---
+++ evaluator/fpg_problem_generation.starified.py
@@ -0,0 +1,244 @@
+import os
+import os.path as osp
+import sys
+import json
+import asyncio
+import traceback
+from datetime import datetime
+from typing import Dict, Set, List, Any, Tuple, Optional
+import pickle
+import regex as re
+import itertools as I
+import random
+
+import vllm     # vLLM should be imported before any 3rd-party libraries, o.w. all Pantograph REPLs are scheduled at the same CPU core.
+from openai import AsyncOpenAI, NOT_GIVEN
+from tqdm import tqdm
+from loguru import logger
+from fire import Fire
+import aiofiles
+
+from common.constants import FPS_GLOBAL_SETTING, CORE_OPTIONS
+from common.utils import add_one_to_port
+from common.pantograph.server import PersistentServer
+from common.pantograph.parsing_server import PersistentParsingServer
+from agent.problem_generation import ProblemFalsifier
+from agent.problem_generation import AutoregressiveProblemGenerationAgent, SFT_LLMAutoregressiveProblemGenerationAgent, SFT_LLMAutoregressiveProblemGenerationAgentV2, SFT_LLMAutoregressiveProblemGenerationAgentV3
+
+NEWLINE = '\n'
+# FPS_GLOBAL_SETTING['TO_SYNC_ENABLED'] = True
+AGENT_DICT: Dict[str, AutoregressiveProblemGenerationAgent] = {
+    'sft_ar' : SFT_LLMAutoregressiveProblemGenerationAgent,
+    'sft_ar_v2': SFT_LLMAutoregressiveProblemGenerationAgentV2,
+    'sft_ar_v3': SFT_LLMAutoregressiveProblemGenerationAgentV3
+}
+
+
+def main(
+    log_root: str,
+    agent_name: str,
+    base_url: str,
+    api_key: str,
+    model_name: str,
+    n_servers: int=1,   # Assuming all servers share the same ip and have consecutive ports
+    falsify_base_url: Optional[str]=None,
+    falsify_api_key: Optional[str]=None,
+    falsify_model_name: Optional[str]=None,
+    falsify_n_servers: int=0,   # Assuming all servers share the same ip and have consecutive ports
+    project_root: str='/home/ma-user/workspace/fps_pantograph/formal_problem_solving/data/MiniF2F',
+    num_generation_attempt: int=5,
+    reassemble_trajectory: bool=False,
+    temperature: float=1.0,
+    max_search_trials: int=80,
+    num_max_samples_per_trial: int=8,
+    max_tokens: int=-1,
+    num_concurrency: int=12,
+    resume_from: Optional[str]=None,
+):
+    saved_args = {**locals()}
+    
+    now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    log_prefix = 'problem_generation'+'.'
+
+    os.makedirs(log_root, exist_ok=True)
+    logger.remove()
+    logger.add(sys.stdout, level='INFO')    # filter=lambda record: record["name"] != "agent.solution_autoformalization"
+    logger.add(osp.join(log_root, log_prefix+now+'.log'), level='DEBUG')
+    logger.info(f'Evaluating problem generator with hyperparams: {saved_args}')
+
+
+    # TODO: Sample `conditions` from ...
+    # In-domain
+    problem_types = ['Algebra', 'Number Theory', 'Precalculus', 'Trigonometry', 'Arithmetic', 'Functional Equations', 'Inequalities', 'Recursion Other', 'Calculus', 'Logic and Puzzles', 'Linear Algebra', 'Combinatorics', 'Other', 'Geometry', 'unknown', 'Intermediate Algebra', 'NaN']
+    sources = ['unknown', 'number_theory', 'aops_forum', 'cn_k12', 'math_train', 'math_test', 'olympiads_ref', 'amc_aime', 'olympiads', 'inequalities', 'secondary_math', 'cn_contest', 'synthetic']
+    
+    # OOD
+    problem_types_ood = ['Abstract Algebra', 'Real Analysis', 'Topology']
+    sources_ood = ['Chinese Gaokao', 'IMO', 'Undergraduate Math Exam', 'Undergraduate Math Textbook', 'Graduate Math Exam', 'Graduate Math Textbook']
+    
+    base_urls = [base_url]
+    for _ in range(n_servers-1):
+        base_urls.append(add_one_to_port(base_urls[-1]))
+    
+    available_falsifiers: List[Tuple[ProblemFalsifier, int]] = []
+    for _ in range(falsify_n_servers):
+        available_falsifiers.append([
+            ProblemFalsifier(
+                clients=[
+                    AsyncOpenAI(
+                        base_url=falsify_base_url,
+                        api_key=falsify_api_key
+                    )
+                ],
+                models=[falsify_model_name],
+                server=PersistentServer(
+                    is_state_based=True,
+                    tag='',
+                    _sync_init=False,
+                    imports=["Mathlib", "Aesop"],
+                    project_path=project_root,
+                    core_options=CORE_OPTIONS,
+                    timeout=300,
+                ),
+                temperature=0.0
+            ),
+            0
+        ])
+        falsify_base_url = add_one_to_port(falsify_base_url)
+    
+    
+    available_servers = [
+        PersistentServer(
+            is_state_based=True,
+            tag='',
+            _sync_init=False,
+            imports=["Mathlib", "Aesop"],
+            project_path=project_root,
+            core_options=CORE_OPTIONS,
+            timeout=300,
+        ) for i in range(num_concurrency)
+    ]
+    available_parsers = [
+        PersistentParsingServer(
+            max_count=32,
+            is_state_based=True,
+            tag='',
+            _sync_init=False,
+            imports=["Mathlib", "Aesop"],
+            project_path=project_root,
+            core_options=CORE_OPTIONS,
+            timeout=300
+        ) for i in range(num_concurrency)
+    ]
+
+    # Load data
+    tasks = list(I.product(
+        I.product(  # Condition
+            I.chain(problem_types, problem_types_ood),
+            I.chain(sources, sources_ood),
+        ), 
+        range(num_generation_attempt)
+    ))
+    random.shuffle(tasks)
+    finished = dict()
+    logger.info(f"Created {len(tasks)} tasks")
+    
+    # Resume from interrupted experiments
+    if resume_from is not None:
+        load_file = sorted([p for p in os.listdir(resume_from) if p.startswith(log_prefix) and p.endswith('.pkl')])
+        if len(load_file) > 0:
+            if len(load_file) > 1:
+                logger.warning(f'Detected multiple checkpoints: {load_file}')
+            load_file = load_file[-1]
+            with open(osp.join(resume_from, load_file), 'rb') as f:
+                finished = pickle.load(f)
+            tasks = [k for k in tasks if k not in finished.keys()]
+            logger.critical(f'Resumed {len(finished)} results from {osp.join(resume_from, load_file)}, now remaining {len(tasks)} tasks to evaluate.')
+
+    async def generate_worker(condition: Any, key: Any, tag_i: int) -> None:
+        server = available_servers.pop()
+        parser = available_parsers.pop()
+        if len(available_falsifiers) > 0:
+            falsifier_and_cnt = available_falsifiers[0]
+            falsifier_and_cnt[1] += 1
+            available_falsifiers.sort(key=lambda x : x[1])  # Load balancing
+        else:
+            falsifier_and_cnt = None
+        
+        try:
+            server.tag = str(tag_i)
+            parser.tag = str(tag_i)
+            client = AsyncOpenAI(
+                base_url=base_urls[tag_i % len(base_urls)],
+                api_key=api_key
+            )
+            problem_generator: AutoregressiveProblemGenerationAgent = AGENT_DICT[agent_name](
+                gen_client=client,
+                gen_model_name=model_name,
+                max_search_trials=max_search_trials,
+                num_max_samples_per_trial=num_max_samples_per_trial,
+                temperature=temperature,
+                max_tokens=(max_tokens if max_tokens > 0 else NOT_GIVEN)
+            )
+            if falsifier_and_cnt is not None:
+                problem_generator.falsifiers.append(
+                    falsifier_and_cnt[0].falsify_async
+                )
+            result = await problem_generator.generate_async(
+                conditions=condition,
+                server=server,
+                parser=parser,
+                reassemble_trajectory=reassemble_trajectory,
+                tag=str(tag_i),
+                verbose=False,
+            )
+            
+            logger.info(f'generate_worker({tag_i}, {condition}): generation finished: {"" if result.header is None else (result.header.rstrip() + NEWLINE)}{result.formal_statement}')
+            finished[key] = result
+        except Exception as e:
+            logger.info(f'generate_worker({tag_i}, {condition}): generation failed due to: {repr(e)}\n{traceback.format_exc()}')
+        finally:
+            server.tag = ''
+            parser.tag = ''
+            available_servers.insert(0, server)
+            available_parsers.insert(0, parser)
+        if len(available_falsifiers) > 0:
+            falsifier_and_cnt[1] -= 1
+            available_falsifiers.sort(key=lambda x : x[1])  # Load balancing
+
+    async def _async_main():
+        pending_tasks: Set[asyncio.Task] = set()
+        for i, (condition, i_generation) in enumerate(tasks):
+            if len(pending_tasks) >= num_concurrency:
+                done_tasks, pending_tasks = await asyncio.wait(pending_tasks, return_when=asyncio.FIRST_COMPLETED)
+                async with aiofiles.open(osp.join(log_root, log_prefix+now+'.pkl'), 'wb') as f:
+                    await f.write(pickle.dumps(finished))
+                for task in done_tasks:
+                    if task.exception() is not None:
+                        logger.error(f"Exception occurred: {task.exception()} {task.get_stack()}")
+                        for pending_task in pending_tasks:
+                            pending_task.cancel()
+                        return
+            pending_tasks.add(
+                asyncio.create_task(
+                    generate_worker(condition, (condition, i_generation), i)
+                )
+            )
+        if len(pending_tasks) > 0:
+            await asyncio.wait(pending_tasks)
+        await logger.complete()
+    
+    try:
+        asyncio.run(_async_main())
+    finally:
+        try:
+            logger.info(f"Finished generation, saving at {osp.join(log_root, log_prefix+now+'.(pkl|jsonl)')}")
+            with open(osp.join(log_root, log_prefix+now+'.pkl'), 'wb') as f:
+                pickle.dump(finished, f)
+        except Exception as e:
+            logger.error(traceback.format_exc())
+            import pdb; pdb.set_trace()
+
+
+if __name__ == '__main__':
+    Fire(main)
